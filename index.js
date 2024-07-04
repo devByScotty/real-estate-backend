da60804724e2473ba01d15b6786b299980024b00
devByScotty <155840062+devByScotty@users.noreply.github.com>
2024-07-04 19:53:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const connectDB = require('../db'); // Adjust path if necessary
+const connectDB = require('./db'); // Adjust path if necessary
 
 const app = express();
 const PORT = process.env.PORT || 3001;
